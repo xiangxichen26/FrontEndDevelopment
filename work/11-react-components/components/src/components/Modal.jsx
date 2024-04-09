72cf14702221c8eb9d6e60d57716fd4ff7fa4858
xiangxichen26 <chen.xiangxi@northeastern.edu>
2024-04-08 23:16:36 -0700

    add validation for the form submition

--- work/11-react-components/components/src/components/Modal.jsx
+++ work/11-react-components/components/src/components/Modal.jsx
@@ -1,66 +1,74 @@
 import React from "react";
-import { useRef,useEffect,useState } from "react";
+import { useRef, useEffect, useState, useId } from "react";
 import Button from "./Button";
 
 import "./Modal.css";
 
 function Modal({ children, title, visible, onClose }) {
-    const dialogRef = useRef(null);
-    const [clicked, setClicked] = useState(false);
-   
-    useEffect(() => {
-      if (visible && dialogRef.current && !dialogRef.current.open) {
-        dialogRef.current.showModal();
-      } else if (!visible && dialogRef.current && dialogRef.current.open) {
-        dialogRef.current.close();
-      }
-    }, [visible]);
-  
-    const handleClose = () => {
-      if (dialogRef.current) {
-        dialogRef.current.close();
-      }
-      if (onClose) {
-        onClose();
-      } 
-    };
+  const dialogRef = useRef(null);
+  const id = useId();
+  const [clicked, setClicked] = useState(false);
+  const [email, setEmail] = useState("");
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        setClicked(true);
-        console.log(`Subscribe Successfully!`);
-        };
+  useEffect(() => {
+    if (visible && dialogRef.current && !dialogRef.current.open) {
+      dialogRef.current.showModal();
+    } else if (!visible && dialogRef.current && dialogRef.current.open) {
+      dialogRef.current.close();
+    }
+  }, [visible]);
+
+  const handleClose = () => {
+    if (dialogRef.current) {
+      dialogRef.current.close();
+    }
+    if (onClose) {
+      onClose();
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setClicked(true);
+    console.log(`Subscribe Successfully!`);
+  };
 
   return (
-      <dialog className="subscribe-dialog" ref={dialogRef} >
-        {clicked && <p>Subscribe Successfully!</p>}
-        <h2 className="subscribe-title">{title}</h2>
-        <form className="subscribe-form">
-          <div className="form-item">
-            <label className="form-label" for="email">
-              <span>Email: </span>
-              <span className="form-label-requiredSign">* Required</span>
-            </label>
-            <div className="form-item-main-content">
-              <input
-                className="form-input"
-                type="text"
-                id="email"
-                name="email"
-              />
-            </div>
-          </div>
-          <div className="form-button">
-            <Button className="subscribe-submit" type="submit" visual="link" onClick={handleSubmit}>
-              Subscribe
-            </Button>
-            <Button className="subscribe-cancel" type="button" visual="link" onClick={handleClose}>
-              close
-            </Button>
-          </div>
-        </form>
-        {children}
-      </dialog>
+    <dialog className="subscribe-dialog" ref={dialogRef}>
+      {clicked && <p>Subscribe Successfully!</p>}
+      <h2 className="subscribe-title">{title}</h2>
+      <form className="subscribe-form" onSubmit={handleSubmit}>
+        <label className="form-label" htmlFor={`${id}-name`}>
+          Email(required):
+        </label>
+        <input
+          className="form-input"
+          type="email"
+          id={`${id}-name`}
+          value={ email }
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <div className="form-button">
+          <Button
+            className="subscribe-submit"
+            type="submit"
+            visual="link"
+          >
+            Subscribe
+          </Button>
+          <Button
+            className="subscribe-cancel"
+            type="button"
+            visual="link"
+            onClick={handleClose}
+          >
+            close
+          </Button>
+        </div>
+      </form>
+      {children}
+    </dialog>
   );
 }
 
